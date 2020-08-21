52e74770ecd30735a9a44f79c28ec71fb6fec727
Steve Wilber <swilber@gmail.com>
2020-08-21 14:13:42 -0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-Trekker-TrekkerUITests/Pods-Trekker-TrekkerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trekker_TrekkerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Trekker_TrekkerUITests
+@end
---
+++ Pods/Target Support Files/Pods-Trekker/Pods-Trekker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trekker : NSObject
+@end
+@implementation PodsDummy_Pods_Trekker
+@end
